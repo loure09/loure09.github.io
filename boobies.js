b6b6d13fdcf391f1df38d141b023c9d6af9885c5
loure09 <oliviaralston@icloud.com>
2025-01-16 02:49:30 +0000

    button and js additions

---
+++ boobies.js
@@ -0,0 +1,8 @@
+// Get the button element
+const button = document.getElementById("linkButton");
+
+// Add the event listener
+button.addEventListener("click", function() {
+  // Code to execute when the button is clicked
+  console.log("Button clicked!");
+});
