52fa71d027cfc85d3f3a5f24ed32b9d291d15246
loure09 <oliviaralston@icloud.com>
2025-01-16 03:00:20 +0000

    work

--- boobies.js
+++ boobies.js
@@ -4,5 +4,5 @@ const button = document.getElementById("linkButton");
 // Add the event listener
 button.addEventListener("click", function() {
   // Code to execute when the button is clicked
-  console.log("Button clicked!");
+  console.log("functional");
 });
