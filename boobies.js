e6ccc2eef173512777e2f7a878efd6d0a9e158dc
loure09 <oliviaralston@icloud.com>
2025-01-17 19:34:13 +0000

    added btbb

--- boobies.js
+++
@@ -1,8 +0,0 @@
-// Get the button element
-const button = document.getElementById("linkButton");
-
-// Add the event listener
-button.addEventListener("click", function() {
-  // Code to execute when the button is clicked
-  console.log("functional");
-});
